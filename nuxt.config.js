b1a438c00ac0114bf8c3d04bc3d1906dec38990c
paul_melero <paulmelero@gmail>
2018-05-21 20:11:58 +0200

    added config.yml and minor changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,11 +14,10 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'isaac2',
+    title: 'isaac',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js + Netlify CMS project' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
