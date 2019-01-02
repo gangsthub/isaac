18b60808875d1196effbd0cce1b9c2fd8066c614
Paul Melero <gangsthub@users.noreply.github.com>
2019-01-02 18:24:22 +0100

    Update ismobile from my mobile

--- middleware/isMobile.js
+++ middleware/isMobile.js
@@ -2,7 +2,7 @@
 // https://github.com/nuxt/nuxt.js/blob/master/examples/dynamic-layouts/middleware/mobile.js
 export default function(ctx) {
   if (ctx.isHMR) return
-  let userAgent = ctx.req ? ctx.req.headers['user-agent'] : navigator.userAgent
+  let userAgent = ctx.req ? ctx.req.headers['user-agent'] : navigator? navigator.userAgent : ''
   ctx.isMobile = /mobile/i.test(userAgent)
   ctx.store.commit('SET_IS_MOBILE', ctx.isMobile)
 }
